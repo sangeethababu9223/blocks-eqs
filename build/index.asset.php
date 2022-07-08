62be5b23e0cb9bd469b08727ab74ac190996618c
Sangeetha Babu <sangeetha.babu@eqs.com>
2022-07-08 18:22:00 +0530

    Richtext

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '00201e6fbad88fb611a0');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '8f0a9d1f8d53a8298460');
--- src/save.js
+++ src/save.js
@@ -3,8 +3,10 @@ import { useBlockProps } from '@wordpress/block-editor';
 
 export default function save() {
 	return (
-		<p { ...useBlockProps.save() }>
-			{ __( 'Blocks Eqs – hello from the saved content!', 'blocks-eqs' ) }
-		</p>
+		<RichText.Content 
+		{ ...useBlockProps.save() } 
+		tagName="h2" 
+		value={ attributes.content } 
+		/>
 	);
 }
